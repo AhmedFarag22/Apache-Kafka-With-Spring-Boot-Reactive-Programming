685f6e97576258cf5c67c36851467166a7a67acd
Ahmed Farag <farag0336@gmail.com>
2025-08-28 19:19:58 +0300

    Finished

---
+++ consumer/src/main/java/com/farag/wikimedia/consumer/consumer/WikimediaConsumer.java
@@ -0,0 +1,21 @@
+package com.farag.wikimedia.consumer.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import static java.lang.String.format;
+
+@Service
+@Slf4j
+public class WikimediaConsumer {
+
+    @KafkaListener(topics = "wikimedia-stream", groupId = "myGroup")
+    public void consumeMsg(String msg) {
+        log.info(format("Consuming the message from farag Topic:: %s", msg));
+    }
+
+
+}
+
+
